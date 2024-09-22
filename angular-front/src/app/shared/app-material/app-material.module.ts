7f16c2992ae040e277dd576b43f8407afed1cb40
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-21 23:27:56 -0300

    matformfield

--- angular-front/src/app/shared/app-material/app-material.module.ts
+++ angular-front/src/app/shared/app-material/app-material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 
@@ -19,7 +19,9 @@ import {MatCardModule} from '@angular/material/card';
     MatDialogModule,
     MatButtonModule,
     MatIconModule,
-    //MatTableModule,
+    MatTableModule,
+    MatFormFieldModule,
+
   ],
 })
 export class AppMaterialModule { }
