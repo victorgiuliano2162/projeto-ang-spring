d6044a96b4d466ddf8e0a69215abe99d8f877e6f
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-21 23:27:41 -0300

    login route

--- angular-front/src/app/app-routing.module.ts
+++ angular-front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'login'
+  },
+  {
+    path: 'login', component: LoginPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
