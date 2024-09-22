a09c3914048a0d25e75587f0c0f2029c1c62cd08
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-21 23:28:06 -0300

    implement shared module to app

---
+++ angular-front/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppMaterialModule } from './app-material/app-material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppMaterialModule,
+  ],
+  exports: [
+    AppMaterialModule,
+  ]
+})
+export class SharedModule { }
