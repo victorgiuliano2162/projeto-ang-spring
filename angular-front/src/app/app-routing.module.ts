1b5e1bf0e1cd575d23ed9082bf7e26a97dd692ba
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-27 18:09:53 -0300

    route for subscribe new user

--- angular-front/src/app/app-routing.module.ts
+++ angular-front/src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: 'login'
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
   },
   {
-    path: 'login', component: LoginPageComponent
-  }
-
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'new',
+    component: RegisterComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
