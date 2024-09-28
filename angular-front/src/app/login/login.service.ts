b3e9de771597d71ff976c5444b105f243c9ba4b8
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-28 10:09:24 -0300

    login service

--- angular-front/src/app/login/login.service.ts
+++ angular-front/src/app/login/login.service.ts
@@ -6,11 +6,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LoginService {
-  private apiUrl = '';
+  private apiUrl = 'http://localhost:8080/api';
 
   constructor(private httpClient: HttpClient) {}
 
-  login(credentials: { email: string, password: string }): Observable<any> {
-    return this.httpClient.post<any>(this.apiUrl, credentials);
+  login(data: any): Observable<any> {
+    console.log("service")
+    return this.httpClient.post<any>(`${this.apiUrl}/`, data);
   }
 }
