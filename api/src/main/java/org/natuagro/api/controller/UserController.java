7f3bd4987044e8f70fdf1d419e47e9287bbb175a
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-30 18:11:44 -0300

    findById

--- api/src/main/java/org/natuagro/api/controller/UserController.java
+++ api/src/main/java/org/natuagro/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.natuagro.api.controller;
 
 
+import org.natuagro.api.model.User;
+import org.natuagro.api.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +13,21 @@ import java.util.Map;
 @CrossOrigin("http://localhost:4200")
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @PostMapping
     public ResponseEntity<String> postData(@RequestBody Object data) {
         return ResponseEntity.ok("Dados recebidos");
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
